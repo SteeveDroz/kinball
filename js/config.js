3484b3c69600cfab696625b9fa7ffeaa38be9b7b
Steeve Droz <steeve.droz@rpn.ch>
2017-09-14 15:56:46 +0200

    Debug a path error

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const config = {}
 const loadConfig = new Promise(function(resolve, reject) {
-    $.getJSON('../config/config.json', function(configData) {
+    $.getJSON('config/config.json', function(configData) {
         const lang = configData.lang
-        $.getJSON(`../config/i18n/${lang}.json`, function(langData) {
+        $.getJSON(`config/i18n/${lang}.json`, function(langData) {
             config.trans = langData
             resolve()
         })
